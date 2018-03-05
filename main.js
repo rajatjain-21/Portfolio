3b41b316ca8c0ec0403bade18983244f6b2208a8
Rajat Jain <rjrox.21@gmail.com>
2018-03-05 19:54:46 +0530

    collapse button fixed partially

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
     $(".navbar a").click(function () {
         $("body,html").animate({scrollTop:$("#"+$(this).data('value')).offset().top},1000)
     })
+    $(document).on('click','.navbar-collapse',function(e) {
+        if( $(e.target).is('a') ) {
+            $(this).collapse('hide');
+        }
+    });
 })
